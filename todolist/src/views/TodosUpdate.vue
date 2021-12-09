cd7f5812309b9bbfeec9de859af961cca70ca935
Brayan Albadam <86798422+albadam94@users.noreply.github.com>
2021-12-08 22:48:06 -0500

    Add files via upload

---
+++ todolist/src/views/TodosUpdate.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="selectedTodo">
+        <todo-form :todo="selectedTodo"
+            @submitForm="saveTodo"
+            submitText="Actualizar todo"
+        ></todo-form>
+    </div>
+    <b-alert show v-else> Todo no identificado </b-alert>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+
+import TodoForm from '../components/TodoForm.vue'
+
+export default {
+    name: 'TodosUpdate',
+    components: { TodoForm },
+    computed: {
+        ...mapState('todos', ['selectedTodo'])
+    },
+    created(){
+        if(!this.selectedTodo){
+            this.fetchTodo(this.$route.params.id);
+        }
+    },
+    methods:{
+        ...mapActions('todos', ['updateTodo', 'fetchTodo']),
+        saveTodo(todo){
+            this.updateTodo(todo).then( () => {
+                this.$router.push({ name: 'Todos' })
+            })
+        }
+    }
+}
+</script>
